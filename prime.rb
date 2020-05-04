21a7d205b52455e97ee6ec6ca9137e5ef4c24c0e
Peter Ort <peter@peterort.com>
2020-05-04 18:24:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,12 @@
 def prime?(num)
   if num == 0 || num <= 1 || num == 1
     return false
+  elsif 
+    (2..num - 1)each.do |n|
+    if num % n == 0
+      return false
+    end
   end
+end
+true
 end
