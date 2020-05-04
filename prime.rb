371144a67940e542bf8cd08a49e1fc9f05e73fb9
Peter Ort <peter@peterort.com>
2020-05-04 18:26:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  range = (2..(number - 1)).to_a
+  if number <= 1
+    return false
+  elsif
+    range.any? do |integer|
+      number % integer == 0
+    end
+    return false
+  else
+    return true
+  end
+end
